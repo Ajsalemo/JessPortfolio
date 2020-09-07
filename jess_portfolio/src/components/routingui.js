4a4a17e0f06a59ef89e7ff429cf9b0356a9bf555
Anthony Salemo <ajssalemo@gmail.com>
2020-09-07 13:26:45 -0400

    added more conditional logic to display icons/location of the icons depending on the current path

--- jess_portfolio/src/components/routingui.js
+++ jess_portfolio/src/components/routingui.js
@@ -3,6 +3,11 @@ import React from "react"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons"
 
 // --------------------------------------- Styled Components ------------------------ //
 // ---------------------------------------------------------------------------------- //
@@ -25,6 +30,7 @@ const StyledRoutingUILink = styled(Link)`
   font-size: 2.1rem;
   font-weight: 500;
   display: flex;
+  align-items: center;
   &:hover {
     color: #000;
     text-decoration: none;
@@ -42,19 +48,31 @@ export const RoutingUI = ({
   start,
   end,
 }) => {
-  console.log(start)
-  console.log(end)
   return (
     <Row>
       <StyledRoutingUICol lg={12} backlink={backLink} start={start} end={end}>
         {backLink ? (
           <StyledRoutingUILink to={backLink}>
-            {backLinkName} 
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              style={{ margin: "0 2rem" }}
+            />
+            {backLinkName}
           </StyledRoutingUILink>
         ) : null}
 
         <StyledRoutingUILink to={forwardLink}>
+          {end ? (
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              style={{ margin: "0 2rem" }}
+            />
+          ) : null}
           {forwardLinkName}
+          <FontAwesomeIcon
+            icon={end ? null : faChevronRight}
+            style={{ margin: "0 2rem" }}
+          />
         </StyledRoutingUILink>
       </StyledRoutingUICol>
     </Row>
