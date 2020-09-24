5739743bcdde7e76e3c117ebc750ab804d55cb5e
Anthony Salemo <ajssalemo@gmail.com>
2020-09-24 18:36:52 -0400

    removed redundant code, made navigation more mobile friendly

--- jess_portfolio/src/components/routingui.js
+++ jess_portfolio/src/components/routingui.js
@@ -14,23 +14,18 @@ import {
 
 // This dictates the 'paginated' type routing link location and how it's displayed based on route
 const StyledRoutingUICol = styled(Col)`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
   align-items: center;
   height: 8rem;
-  @media (min-width: 992px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: ${props =>
-      props.backlink
-        ? "space-between"
-        : props.start
-        ? "flex-end"
-        : props.end
-        ? "flex-start"
-        : null};
-  }
+  display: flex;
+  flex-direction: row;
+  justify-content: ${props =>
+    props.backlink
+      ? "space-between"
+      : props.start
+      ? "flex-end"
+      : props.end
+      ? "flex-start"
+      : null};
 `
 
 const StyledRoutingUILink = styled(Link)`
@@ -45,6 +40,13 @@ const StyledRoutingUILink = styled(Link)`
   }
   @media (min-width: 602px) {
     font-size: 2.1rem;
+  }
+`
+
+const StyledRoutingUISpan = styled.span`
+  display: none;
+  @media (min-width: 992px) {
+    display: initial;
   }
 `
 
@@ -74,7 +76,7 @@ export const RoutingUI = ({
               icon={faChevronLeft}
               style={{ margin: "0 2rem" }}
             />
-            {backLinkName}
+            <StyledRoutingUISpan>{backLinkName}</StyledRoutingUISpan>
           </StyledRoutingUILink>
         ) : null}
 
@@ -85,7 +87,7 @@ export const RoutingUI = ({
               style={{ margin: "0 2rem" }}
             />
           ) : null}
-          {forwardLinkName}
+          <StyledRoutingUISpan>{forwardLinkName}</StyledRoutingUISpan>
           {end ? null : (
             <FontAwesomeIcon
               icon={faChevronRight}
